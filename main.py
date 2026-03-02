5be2b12dc00a1c1987f3f647c2e153990a08f0bd
choiguen <daindj0@gmail.com>
2026-03-03 00:05:55 +0900

    Feat: 유저 생성, 조회 endpoint 추가

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from app.db.base import engine, Base
+from app.schemas.user import UserCreate, UserResponse
+from app.models.user import User
+from fastapi import Body, Depends
+from app.db.session import get_db
+from sqlalchemy.orm import Session
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "루트 페이지임."
+@app.post("/user/", response_model=UserResponse)
+def root(user: UserCreate = Body(...), db: Session = Depends(get_db)):
+    user = User(username=user.username, email=user.email, password=user.password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
+@app.get("/user/")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
